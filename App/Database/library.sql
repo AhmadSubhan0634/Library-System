371712d45a3e7685c744d4181c6df891821019f6
AhmadSubhan0634 <f240634@lhr.nu.edu.pk>
2026-07-04 10:45:47 +0500

    Add constraint for uniqueness of email in authors table

--- App/Database/library.sql
+++ App/Database/library.sql
@@ -60,3 +60,4 @@ updated_at date
 alter table books add constraint c1 foreign key(author_id) references authors(id);
 alter table books add constraint c2 foreign key(category_id) references category(id);
 alter table books add constraint c3 unique(isbn);
+alter table authors add constraint c4 unique(email);
